d7d56b2df826ccb09d9b50612ed7b950bf7b3cce
Axel Nowaczyk <axelnowaczyk@gmail.com>
2016-04-07 21:50:10 +0200

    initial commit

---
+++ SnakeGame/SnakeGame.swift
@@ -0,0 +1,20 @@
+//
+//  SnakeGame.swift
+//  SnakeGame
+//
+//  Created by Axel Nowaczyk on 07.04.2016.
+//  Copyright © 2016 Axel Nowaczyk. All rights reserved.
+//
+
+import Foundation
+
+public struct Settings{
+    var speed           = 4 // from 1 till 10
+    var bestScore       = 0
+    var difficultyLvl   = 1 // from 1 till 5
+    let heightOfBoard   = 40
+    let widthOfBoard    = 20
+}
+class SnakeGame {
+    
+}
